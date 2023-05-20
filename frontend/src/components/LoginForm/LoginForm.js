4ef39c28454d6d99babc1e1b089afe1882d9fa91
nguyenhieu12 <vailinhhon098@gmail.com>
2023-05-20 21:44:39 +0700

    login form completed

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -1,5 +1,7 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './LoginForm.css';
+import userIcon from '../../assets/icons/user-icon.png';
+import passwordIcon from '../../assets/icons/password-icon.png';
 
 function LoginForm({ closeForm }) {
   const formRef = useRef();
@@ -22,10 +24,46 @@ function LoginForm({ closeForm }) {
     event.stopPropagation();
   };
 
+  // const [isFocused, setIsFocused] = useState(false);
+
+  // const handleFocus = () => {
+  //   setIsFocused(true);
+  // };
+
+  // const handleBlur = () => {
+  //   setIsFocused(false);
+  // };
+
   return (
     <div className='login-container' ref={formRef} onClick={closeForm}>
-      <div className="login-content" onClick={handleClickInside}>
-
+      <div className="login-background" onClick={handleClickInside}>
+        <div className='login-form'>
+          <h2>Chào mừng</h2>
+          <div className='input-wrapper'>
+            <img src={userIcon} />
+            <input 
+              type='text'
+              className='input-field'
+              placeholder='Tên đăng nhập'
+            />
+          </div>
+          <hr/>
+          <div className='input-wrapper'>
+            <img src={passwordIcon} />
+            <input 
+              type='password'
+              className='input-field'
+              placeholder='Mật khẩu'
+            />
+          </div>
+          <hr/>
+          <div className='login-options'>
+            <input type='checkbox'/>
+            <span>Ghi nhớ đăng nhập</span>
+            <span>Quên mật khẩu?</span>
+          </div>
+          <button className='login'>Đăng nhập</button>
+        </div>
       </div>
     </div>
   );
