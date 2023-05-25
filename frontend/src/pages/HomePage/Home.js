9edd9c303c943385ac2a12e9dacc83240d336024
nguyenhieu12 <vailinhhon098@gmail.com>
2023-05-25 10:27:34 +0700

    navigate home to services bookmark

--- frontend/src/pages/HomePage/Home.js
+++ frontend/src/pages/HomePage/Home.js
@@ -4,96 +4,89 @@ import homeBackground1 from '../../assets/images/home-background1.png';
 import homeBackground2 from '../../assets/images/home-background2.png';
 import homeBackground3 from '../../assets/images/home-background3.png';
 import Navbar from '../../components/Navbar/Navbar';
-import Developers from '../DevelopersPage/Developers';
-import Services from '../ServicesPage/Services';
-import News from '../NewsPage/News';
-import Search from '../SearchPage/Search';
+import {useNavigate} from "react-router-dom";
 
 function Home() {
-  const [activeIndex, setActiveIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % 3);
-    }, 3500);
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActiveIndex((prevIndex) => (prevIndex + 1) % 3);
+        }, 3500);
+        return () => clearInterval(interval);
+    }, []);
 
-  const [isFormOpened, setisFormOpened] = useState(false); 
+    const [isFormOpened, setIsFormOpened] = useState(false);
 
-  const handleOpenForm = () => {
-    setisFormOpened(true)
-  }
+    const handleOpenForm = () => {
+        setIsFormOpened(true);
+    };
 
-  const handleCloseForm = () => {
-    setisFormOpened(false)
-  }
+    const handleCloseForm = () => {
+        setIsFormOpened(false);
+    };
 
-  const [tabSelected, setTabSelected] = useState(1);
+    const navigate = useNavigate();
 
-  function handleTabSelected(tab) {
-    setTabSelected(tab);
-  }
+    const handleLearnMore = (path, containerId) => {
+        navigate(path + '#' + containerId);
+    };
 
-  return (
-    <div className='home-container' >
-      <div
-        className="home-slide"
-        style={{ transform: `translateX(-${activeIndex * 100}vw)` }}
-      >
-        <Home1 />
-        <Home2 />
-        <Home3 />
-      </div>
-      <Navbar style={1} isOpen={isFormOpened} handleLoginClick={handleOpenForm} handleOutsideClick={handleCloseForm}/>
-    </div>
-  );
-};
-
-function Home1() {
-  return (
-    <div className="home1">
-      <div id="page1" className="page-content">
-        <h1>Đảm bảo an toàn</h1>
-        <h1>phương tiện</h1>
-        <p>Đảm bảo phương tiện luôn được kiểm tra</p>
-        <p>đúng hạn trước những hành trình dài</p>
-        <button>Xem thêm</button>
-      </div>
-      <img src={homeBackground1} alt="Home 1" />
-    </div>
-  );
+    return (
+        <div className="home-container">
+            <div className="home-slide" style={{ transform: `translateX(-${activeIndex * 100}vw)` }}>
+                <Home1 handleLearnMore={handleLearnMore} />
+                <Home2 handleLearnMore={handleLearnMore} />
+                <Home3 handleLearnMore={handleLearnMore} />
+            </div>
+            <Navbar style={1} isOpen={isFormOpened} handleLoginClick={handleOpenForm} handleOutsideClick={handleCloseForm} />
+        </div>
+    );
 }
 
-function Home2() {
-  return (
-    <div className="home2">
-      <div id="page2" className="page-content">
-        <h1>Truy xuất thông tin</h1>
-        <h1>dễ dàng</h1>
-        <p>Truy xuất dữ liệu đăng kiểm theo quý, năm</p>
-        <p>một cách nhanh chóng và dễ dàng</p>
-        <button>Xem thêm</button>
-      </div>
-      <img src={homeBackground2} alt="Home 2" />
-    </div>
-  );
+function Home1({ handleLearnMore }) {
+    return (
+        <div className="home1">
+            <div id="page1" className="page-content">
+                <h1>Đảm bảo an toàn</h1>
+                <h1>phương tiện</h1>
+                <p>Đảm bảo phương tiện luôn được kiểm tra</p>
+                <p>đúng hạn trước những hành trình dài</p>
+                <button onClick={() => handleLearnMore('/services', 'safe')}>Xem thêm</button>
+            </div>
+            <img src={homeBackground1} alt="Home 1" />
+        </div>
+    );
 }
 
-function Home3() {
-  return (
-    <div className="home3">
-      <div id="page3" className="page-content">
-        <h1>Lưu trữ dữ liệu</h1>
-        <h1>hiệu quả</h1>
-        <p>Dữ liệu được lưu trữ an toàn trong cơ sở</p>
-        <p>dữ liệu của cục đăng kiểm</p>
-        <button>Xem thêm</button>
-      </div>
-      <img src={homeBackground3} alt="Home 3" />
-    </div>
-  );
+function Home2({ handleLearnMore }) {
+    return (
+        <div className="home2">
+            <div id="page2" className="page-content">
+                <h1>Truy xuất thông tin</h1>
+                <h1>dễ dàng</h1>
+                <p>Truy xuất dữ liệu đăng kiểm theo quý, năm</p>
+                <p>một cách nhanh chóng và dễ dàng</p>
+                <button onClick={() => handleLearnMore('/services', 'certificate')}>Xem thêm</button>
+            </div>
+            <img src={homeBackground2} alt="Home 2" />
+        </div>
+    );
 }
 
+function Home3({ handleLearnMore }) {
+    return (
+        <div className="home3">
+            <div id="page3" className="page-content">
+                <h1>Lưu trữ dữ liệu</h1>
+                <h1>hiệu quả</h1>
+                <p>Dữ liệu được lưu trữ an toàn trong cơ sở</p>
+                <p>dữ liệu của cục đăng kiểm</p>
+                <button onClick={() => handleLearnMore('/services', 'storage')}>Xem thêm</button>
+            </div>
+            <img src={homeBackground3} alt="Home 3" />
+        </div>
+    );
+}
 export default Home;
 
--- frontend/src/pages/ServicesPage/Services.js
+++ frontend/src/pages/ServicesPage/Services.js
@@ -1,58 +1,92 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import './Services.css'
 import carIcon from '../../assets/icons/car.png';
 import certifiIcon from '../../assets/icons/certification.png';
 import dbIcon from '../../assets/icons/database.png';
 import easyIcon from '../../assets/icons/easy.png';
+import {useNavigate, useLocation} from "react-router-dom";
 
 function Services() {
-    const [isFormOpened, setisFormOpened] = useState(false); 
+    const [isFormOpened, setIsFormOpened] = useState(false);
 
     const handleOpenForm = () => {
-        setisFormOpened(true)
-    }
+        setIsFormOpened(true);
+    };
 
     const handleCloseForm = () => {
-        setisFormOpened(false)
-    }
+        setIsFormOpened(false);
+    };
 
-    return(
+    const navigate = useNavigate();
+
+    const scrollToContainer = (containerId) => {
+        const container = document.getElementById(containerId);
+        if (container) {
+            container.scrollIntoView({ behavior: 'smooth' });
+            navigate(`#${containerId}`);
+        }
+    };
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const containerId = location.hash.substr(1);
+        const container = document.getElementById(containerId);
+        if (container) {
+            container.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [location]);
+
+    return (
         <div className="services-container">
             <h1 className="services-heading">Các dịch vụ được cung cấp</h1>
-                <div className="services-options">
-                    <div className="options option1">
-                        <img src={carIcon} alt="car"/>
-                        <h2>Đảm bao an toàn</h2>
-                        <p>Phân chia quyền sử dụng</p>
-                        <h3>Xem thêm</h3>
-                    </div>  
-                    <div className="options option2">
-                        <img src={certifiIcon} alt="certification"/>
-                        <h2>Chứng nhận nhanh chóng</h2>
-                        <p>Ghi nhận kết quả nhanh chóng</p>
-                        <h3>Xem thêm</h3>
-                    </div>
-                    <div className="options option3">
-                        <img src={dbIcon} alt="database"/>
-                        <h2>Lưu trữ hiệu quả</h2>
-                        <p>Dữ liệu được lưu trữ hiệu quả</p>
-                        <h3>Xem thêm</h3>
-                    </div>
-                    <div className="options option4">
-                        <img src={easyIcon} alt="easy"/>
-                        <h2>Dễ dàng sử dụng</h2>
-                        <p>Thân thiện với người dùng</p>
-                        <h3>Xem thêm</h3>
-                    </div>
+            <div className="services-options">
+                <div className="options option1" onClick={() => scrollToContainer('safe')}>
+                    <img src={carIcon} alt="car" />
+                    <h2>Đảm bảo an toàn</h2>
+                    <p>Phân chia quyền sử dụng</p>
+                    <h3>Xem thêm</h3>
                 </div>
-                <div className="services-navbar">
-                    <Navbar style={3}
-                        isOpen={isFormOpened}
-                        handleLoginClick={handleOpenForm}
-                        handleOutsideClick={handleCloseForm}
-                    />
+                <div className="options option2" onClick={() => scrollToContainer('certificate')}>
+                    <img src={certifiIcon} alt="certification" />
+                    <h2>Chứng nhận nhanh chóng</h2>
+                    <p>Ghi nhận kết quả nhanh chóng</p>
+                    <h3>Xem thêm</h3>
                 </div>
+                <div className="options option3" onClick={() => scrollToContainer('storage')}>
+                    <img src={dbIcon} alt="database" />
+                    <h2>Lưu trữ hiệu quả</h2>
+                    <p>Dữ liệu được lưu trữ hiệu quả</p>
+                    <h3>Xem thêm</h3>
+                </div>
+                <div className="options option4" onClick={() => scrollToContainer('tutorial')}>
+                    <img src={easyIcon} alt="easy" />
+                    <h2>Dễ dàng sử dụng</h2>
+                    <p>Thân thiện với người dùng</p>
+                    <h3>Xem thêm</h3>
+                </div>
+            </div>
+            <div className="services-navbar">
+                <Navbar
+                    style={3}
+                    isOpen={isFormOpened}
+                    handleLoginClick={handleOpenForm}
+                    handleOutsideClick={handleCloseForm}
+                />
+            </div>
+            <div className="safe" id="safe">
+                Hello 1
+            </div>
+            <div className="certificate" id="certificate">
+                Hello 2
+            </div>
+            <div className="storage" id="storage">
+                Hello 3
+            </div>
+            <div className="tutorial" id="tutorial">
+                Hello 4
+            </div>
         </div>
     );
 }
