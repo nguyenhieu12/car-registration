2fcfb64c084a78e472d50399c4f0bd04b7568255
NguyenKhaiHoan <banghoan1511@gmail.com>
2023-06-02 15:48:36 +0700

    update layout dashboard, fix tabs

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,148 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import DashboardContent from '../../components/DashboardContent/DashboardContent';
-// import ProfileContent from '../../components/ProfileContent/ProfileContent';
-// import NewsContent from '../../components/NewsContent/NewsContent';
-// import './Dashboard.css';
-// import 'boxicons/css/boxicons.min.css';
-// import SettingContent from '../../components/SettingContent/SettingContent';
-
-// function Dashboard(props) {
-
-//     const [currentId, setCurrentId] = useState(0);
-
-//     const [tabs, setTabs] = useState([
-//         { id: 0, title: 'Dashboard', content: <DashboardContent />, activeLink: true },
-//     ]);
-
-//     const [activeTab, setActiveTab] = useState(tabs[0].id);
-
-//     const addTab = (title, content) => {
-//         const existingTab = tabs.find((tab) => tab.title === title);
-//         if (existingTab) {
-//             setActiveTab(existingTab.id);
-//             return;
-//         }
-//         const newTab = {
-//             id: currentId + 1,
-//             title: title,
-//             content: content,
-//             activeLink: false,
-//         };
-//         setCurrentId(currentId + 1);
-//         setTabs([...tabs, newTab]);
-//         setActiveTab(newTab.id);
-//     };
-
-//     const removeTab = (id) => {
-//         if (tabs.length === 1) {
-//             return;
-//         }
-
-//         let updatedTabs = [];
-
-//         if (id === activeTab) {
-//             const index = tabs.findIndex(tab => tab.id === activeTab);
-//             if (index > 0 && index < tabs.length) {
-//                 setActiveTab(tabs[index - 1].id)
-//             } else if (index === 0) {
-//                 setActiveTab(tabs[0].id)
-//             } else if (index === tabs.length) {
-//                 setActiveTab(tabs[tabs.length - 1].id)
-//             }
-//             updatedTabs = tabs.filter((tab) => tab.id !== id);
-//             setTabs(updatedTabs);
-//             return;
-//         } else {
-//             updatedTabs = tabs.filter((tab) => tab.id !== id);
-//             setTabs(updatedTabs);
-//             setActiveTab(activeTab);
-//             return;
-//         }
-//     };
-
-//     const [isNavOpen, setIsNavOpen] = useState(false);
-
-//     const toggleNavbar = () => {
-//         setIsNavOpen(!isNavOpen);
-//     };
-
-//     return (
-//         <div className="dashboard-container">
-//             <header className="header-container" id="header">
-//                 <div className="header__toggle" onClick={toggleNavbar}>
-//                     <i className={isNavOpen ? 'bx bx-x' : 'bx bx-menu'} style={{ transform: isNavOpen ? 'translateX(170px)' : 'translateX(0)' }} id="header-toggle"></i>
-//                 </div>
-
-//                 <div className="header__img">
-//                     <img src="https://c.pxhere.com/photos/dd/a6/business_man_businessman_corporate_suit_executive_successful_entrepreneur-675843.jpg!d" alt="" />
-//                 </div>
-//             </header>
-
-//             <div className={`l-navbar ${isNavOpen ? 'show' : ''}`} id="nav-bar">
-//                 <nav className="nav-container">
-//                     <div>
-//                         <a href="#" className="nav__logo">
-//                             <i className="bx bxs-car-garage nav__logo-icon"></i>
-//                             <span className="nav__logo-name">Bedimcode</span>
-//                         </a>
-
-//                         <div className="nav__list">
-//                             <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Dashboard') ? 'isActive' : ''}`} onClick={() => addTab('Dashboard', <DashboardContent />)}>
-//                                 <i className="bx bx-grid-alt nav__icon"></i>
-//                                 <span className="nav__name">Dashboard</span>
-//                             </a>
-
-//                             <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Thông tin cá nhân') ? 'isActive' : ''}`} onClick={() => addTab('Thông tin cá nhân', <ProfileContent />)}>
-//                                 <i className="bx bx-user nav__icon"></i>
-//                                 <span className="nav__name">Thông tin cá nhân</span>
-//                             </a>
-
-//                             <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Quản lý tin tức') ? 'isActive' : ''}`} onClick={() => addTab('Quản lý tin tức', <NewsContent />)}>
-//                                 <i className="bx bx-news nav__icon"></i>
-//                                 <span className="nav__name">Quản lý tin nhắn</span>
-//                             </a>
-
-//                             <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Cài đặt') ? 'isActive' : ''}`} onClick={() => addTab('Cài đặt', <SettingContent />)}>
-//                                 <i className="bx bxs-cog nav__icon"></i>
-//                                 <span className="nav__name">Cài đặt</span>
-//                             </a>
-//                         </div>
-//                     </div>
-
-//                     <a href="#" className="nav__link">
-//                         <i className="bx bx-log-out nav__icon"></i>
-//                         <span className="nav__name">Đăng xuất</span>
-//                     </a>
-//                 </nav>
-//             </div>
-
-//             <main>
-//                 <div className="tab-container">
-//                     <div className="tab-header">
-//                         {tabs.map((tab) => (
-//                             <div
-//                                 className={`tab-item ${tab.id === activeTab ? 'active' : ''}`}
-//                                 key={tab.id}>
-//                                 <p className='tab-title' onClick={() => setActiveTab(tab.id)}>{tab.title}</p>
-//                                 {tabs.length > 1 && (
-//                                     <span className="tab-close" onClick={() => removeTab(tab.id)}>
-//                                         &times;
-//                                     </span>
-//                                 )}
-//                             </div>
-//                         ))}
-//                     </div>
-//                     <div className="tab-content">
-//                         {tabs.find((tab) => tab.id === activeTab)?.content}
-//                     </div>
-//                 </div>
-//             </main>
-//         </div>
-//     );
-// }
-
-// export default Dashboard;
-
 import React, { useState, useEffect } from 'react';
 import DashboardContent from '../../components/DashboardContent/DashboardContent';
 import ProfileContent from '../../components/ProfileContent/ProfileContent';
@@ -150,32 +5,43 @@ import NewsContent from '../../components/NewsContent/NewsContent';
 import './Dashboard.css';
 import 'boxicons/css/boxicons.min.css';
 import SettingContent from '../../components/SettingContent/SettingContent';
+import SearchContent from '../../components/SearchContent/SearchContent';
 
 function Dashboard(props) {
-
-    const [currentId, setCurrentId] = useState(0);
-
     const [tabs, setTabs] = useState([
-        { id: 0, title: 'Dashboard', content: <DashboardContent />, activeLink: true },
+        { id: Date.now(), title: 'Dashboard', content: <DashboardContent />, iconName: 'bx bxs-dashboard' },
     ]);
 
     const [activeTab, setActiveTab] = useState(tabs[0].id);
 
-    const addTab = (title, content) => {
+    const [contentTabs, setContentTabs] = useState([
+        {
+            id: tabs[0].id,
+            content: <DashboardContent />,
+        },
+    ]);
+
+    const addTab = (title, content, iconName) => {
         const existingTab = tabs.find((tab) => tab.title === title);
         if (existingTab) {
             setActiveTab(existingTab.id);
             return;
         }
         const newTab = {
-            id: currentId + 1,
+            id: Date.now(),
             title: title,
             content: content,
-            activeLink: false,
+            iconName: iconName,
         };
-        setCurrentId(currentId + 1);
+
         setTabs([...tabs, newTab]);
         setActiveTab(newTab.id);
+
+        const newContentTab = {
+            id: newTab.id,
+            content: content,
+        };
+        setContentTabs([...contentTabs, newContentTab]);
     };
 
     const removeTab = (id) => {
@@ -184,6 +50,7 @@ function Dashboard(props) {
         }
 
         let updatedTabs = [];
+        let updatedContentTabs = [];
 
         if (id === activeTab) {
             const index = tabs.findIndex(tab => tab.id === activeTab);
@@ -196,92 +63,125 @@ function Dashboard(props) {
             }
             updatedTabs = tabs.filter((tab) => tab.id !== id);
             setTabs(updatedTabs);
+
+            updatedContentTabs = contentTabs.filter((tab) => tab.id !== id);
+            setContentTabs(updatedContentTabs);
             return;
         } else {
             updatedTabs = tabs.filter((tab) => tab.id !== id);
             setTabs(updatedTabs);
             setActiveTab(activeTab);
+
+            updatedContentTabs = contentTabs.filter((tab) => tab.id !== id);
+            setContentTabs(updatedContentTabs);
             return;
         }
     };
 
-    const [isNavOpen, setIsNavOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-    const toggleNavbar = () => {
-        setIsNavOpen(!isNavOpen);
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
     };
 
     return (
-        <div className="dashboard-container">
-            <header className="header" id="header">
-                <div className="header__toggle" onClick={toggleNavbar}>
-                    <i className={isNavOpen ? 'bx bx-x' : 'bx bx-menu'} style={{ transform: isNavOpen ? 'translateX(170px)' : 'translateX(0)' }} id="header-toggle"></i>
+        <div className='dashboard-container'>
+            <div className={`sidebar-container ${isOpen ? 'open' : ''}`}>
+                <div className="logo-details">
+                    <i className="bx bxl-c-plus-plus icon"></i>
+                    <div className="logo_name">Hệ thống đăng kiểm</div>
+                    <i
+                        className={`bx ${isOpen ? 'bx bx-x' : 'bx-menu'}`}
+                        id="btn"
+                        onClick={handleToggle}
+                    ></i>
                 </div>
-
-                <div className="header__img">
-                    <img src="https://c.pxhere.com/photos/dd/a6/business_man_businessman_corporate_suit_executive_successful_entrepreneur-675843.jpg!d" alt="" />
-                </div>
-            </header>
-
-            <div className={`l-navbar ${isNavOpen ? 'show' : ''}`} id="nav-bar">
-                <nav className="nav">
-                    <div>
-                        <a href="#" className="nav__logo">
-                            <i className="bx bx-layer nav__logo-icon"></i>
-                            <span className="nav__logo-name">Bedimcode</span>
+                <ul className="sidebar-list">
+                    <li>
+                        <a href="#" className={`.sidebar-container li a ${tabs.find(tab => tab.title === 'Dashboard') ? 'isActive' : ''}`} onClick={() => addTab('Dashboard', <DashboardContent />, 'bx bxs-dashboard')}>
+                            <i class='bx bxs-dashboard' ></i>
+                            <span className="sidebar-links">Dashboard</span>
                         </a>
-
-                        <div className="nav__list">
-                            <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Dashboard') ? 'active' : ''}`} onClick={() => addTab('Dashboard', <DashboardContent />)}>
-                                <i className="bx bx-grid-alt nav__icon"></i>
-                                <span className="nav__name">Dashboard</span>
-                            </a>
-
-                            <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Thông tin cá nhân') ? 'active' : ''}`} onClick={() => addTab('Thông tin cá nhân', <ProfileContent />)}>
-                                <i className="bx bx-user nav__icon"></i>
-                                <span className="nav__name">Thông tin cá nhân</span>
-                            </a>
-
-                            <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Quản lý tin tức') ? 'active' : ''}`} onClick={() => addTab('Quản lý tin tức', <NewsContent />)}>
-                                <i className="bx bx-news nav__icon"></i>
-                                <span className="nav__name">Quản lý tin nhắn</span>
-                            </a>
-
-                            <a href="#" className={`nav__link ${tabs.find(tab => tab.title === 'Cài đặt') ? 'active' : ''}`} onClick={() => addTab('Cài đặt', <SettingContent />)}>
-                                <i className="bx bxs-cog nav__icon"></i>
-                                <span className="nav__name">Cài đặt</span>
-                            </a>
-                        </div>
-                    </div>
-
-                    <a href="#" className="nav__link">
-                        <i className="bx bx-log-out nav__icon"></i>
-                        <span className="nav__name">Đăng xuất</span>
-                    </a>
-                </nav>
+                        <span className="tooltip">Dashboard</span>
+                    </li>
+                    <li>
+                        <a href="#" className={`.sidebar-container li a ${tabs.find(tab => tab.title === 'Tra cứu') ? 'isActive' : ''}`} onClick={() => addTab('Tra cứu', <SearchContent />, 'bx bx-search-alt')}>
+                            <i class='bx bx-search-alt' ></i>
+                            <span className="sidebar-links">Tra cứu</span>
+                        </a>
+                        <span className="tooltip">Tra cứu</span>
+                    </li>
+                    <li>
+                        <a href="#" className={`.sidebar-container li a ${tabs.find(tab => tab.title === 'Thông tin cá nhân') ? 'isActive' : ''}`} onClick={() => addTab('Thông tin cá nhân', <ProfileContent />, 'bx bx-user')}>
+                            <i class='bx bx-user' ></i>
+                            <span className="sidebar-links">Thông tin cá nhân</span>
+                        </a>
+                        <span className="tooltip">Thông tin cá nhân</span>
+                    </li>
+                    <li>
+                        <a href="#" className={`.sidebar-container li a ${tabs.find(tab => tab.title === 'Quản lý tin tức') ? 'isActive' : ''}`} onClick={() => addTab('Quản lý tin tức', <NewsContent />, 'bx bx-news')}>
+                            <i class='bx bx-news' ></i>
+                            <span className="sidebar-links">Quản lý tin tức</span>
+                        </a>
+                        <span className="tooltip">Quản lý tin tức</span>
+                    </li>
+                    <li>
+                        <a href="#" className={`.sidebar-container li a ${tabs.find(tab => tab.title === 'Setting') ? 'isActive' : ''}`} onClick={() => addTab('Cài đặt', <SettingContent />, 'bx bx-cog')}>
+                            <i class='bx bx-cog' ></i>
+                            <span className="sidebar-links">Cài đặt</span>
+                        </a>
+                        <span className="tooltip">Cài đặt</span>
+                    </li>
+                    <li className='logout'>
+                        <a href="#">
+                            <i class='bx bx-log-out' ></i>
+                            <span className="sidebar-links">Đăng xuất</span>
+                        </a>
+                        <span className="tooltip">Đăng xuất</span>
+                    </li>
+                </ul>
             </div>
 
-            <main>
+            <section class="main-container">
+                <div className="main-container-header">
+                    <div className='welcome'>
+                        <p>Chào mừng quay trở lại,</p>
+                        <h2 className='user-name-title'>Hoàn Bằng</h2>
+                    </div>
+                    <div className="user-info">
+                        <div className="search-box">
+                            <i class='bx bx-search'></i>
+                            <input type="text" placeholder="Search" />
+                        </div>
+                        <img
+                            src="https://scontent.fhan14-4.fna.fbcdn.net/v/t1.6435-9/182741929_107870648128506_6644574165595211881_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5kAhAr4PEkoAX-zfreP&_nc_ht=scontent.fhan14-4.fna&oh=00_AfCJqeKHCh0PvtaZ9Y9AiY_0vUc6X8QkOtlc9loxucFV0w&oe=649DFFEE"
+                            alt=""
+                        />
+                    </div>
+                </div>
+
                 <div className="tab-container">
                     <div className="tab-header">
                         {tabs.map((tab) => (
                             <div
                                 className={`tab-item ${tab.id === activeTab ? 'active' : ''}`}
                                 key={tab.id}>
+                                <i className={tab.iconName}></i>
                                 <p className='tab-title' onClick={() => setActiveTab(tab.id)}>{tab.title}</p>
                                 {tabs.length > 1 && (
                                     <span className="tab-close" onClick={() => removeTab(tab.id)}>
                                         &times;
                                     </span>
                                 )}
+                                { }
                             </div>
                         ))}
                     </div>
                     <div className="tab-content">
-                        {tabs.find((tab) => tab.id === activeTab)?.content}
+                        {contentTabs.find((tab) => tab.id === activeTab)?.content}
                     </div>
                 </div>
-            </main>
+            </section>
         </div>
     );
 }
