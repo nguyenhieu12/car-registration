a6b1e70781f11d8e371c433ddd3f26349dfcf375
NguyenKhaiHoan <banghoan1511@gmail.com>
2023-06-11 04:27:12 +0700

    update view inspection report, export pdf

--- frontend/src/components/SearchContent/SearchContent.js
+++ frontend/src/components/SearchContent/SearchContent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { utils, read } from 'xlsx';
 import './SearchContent.css';
 import 'boxicons/css/boxicons.min.css';
@@ -8,6 +8,7 @@ import "primereact/resources/primereact.min.css";
 import 'boxicons/css/boxicons.min.css';
 import { FilterMatchMode } from 'primereact/api';
 import { InputText } from 'primereact/inputtext';
+import { useReactToPrint } from 'react-to-print';
 
 function SearchContent(props) {
     const [excelData, setExcelData] = useState([]);
@@ -57,11 +58,21 @@ function SearchContent(props) {
     const actionTemplate = (rowData) => {
         return (
             <div>
-                <i className="bx bx-edit-alt" onClick={() => alertMessage(rowData)} style={{ cursor: 'pointer' }}></i>
+                <i className="bx bx-edit-alt" onClick={() => triggerModal(rowData)} style={{ cursor: 'pointer' }}></i>
                 <i className="bx bx-trash-alt" onClick={() => handleDeleteRow(rowData)} style={{ cursor: 'pointer', marginLeft: '0.5rem' }}></i>
             </div>
         );
     };
+
+    const [modal, setModal] = useState(false);
+    const triggerModal = (rowData) => {
+        setModal(!modal);
+    };
+
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+    });
 
     return (
         <div className='searchContent-container'>
@@ -92,6 +103,121 @@ function SearchContent(props) {
                         <Column field='place_of_registration' header='Nơi đăng ký' sortable/>
                         <Column body={actionTemplate} header="" style={{ textAlign: 'center', width: '6rem' }} />
                     </DataTable>
+                </div>
+                <div className="container">
+                    {modal && (
+                        <div ref={componentRef} className="popup">
+                            <div onClick={triggerModal} className="overlay"></div>
+                            <div className="modal-content" id="inspection-report">
+                            <form className="form">
+                                <div className='form-row-popup'>
+                                    <div className='form-row-popup-item outline'>
+                                        <h3>1. PHƯƠNG TIỆN <span className="big-text">(VEHICLE)</span></h3>
+                                        <div className='row-flex'>
+                                        <div className='row-flex-item'>
+                                            <p>Biển đăng ký: 51F-516.04<br/>
+                                            <span className="small-text">(Registration Number)</span></p>
+                                        </div>
+                                        <div className='row-flex-item'>
+                                            <p>Số quản lý: 5005V-122752<br/>
+                                            <span className="small-text">(Vehicle Inspection No.)</span></p>
+                                        </div>
+                                        </div>
+                                        <p>Loại phương tiện: <span className="small-text">(Type)</span> ô tô con</p>
+                                        <p>Nhãn hiệu: <span className="small-text">(Mark)</span> KIA</p>
+                                        <p>Số loại: <span className="small-text">(Model code)</span> MORNING TA 12G E2 AT-1</p>
+                                        <p>Số khung: <span className="small-text">(Chassis Number)</span> RNYTC51A4FC066108</p>
+                                        <div className='row-flex'>
+                                        <div className='row-flex-item'>
+                                            <p>Năm, Nước sản xuất: 2015, Việt Nam<br/>
+                                            <span className="small-text">(Manufactured Year and Country)</span></p>
+                                        </div>
+                                        <div className='row-flex-item'>
+                                            <p>Năm hết niên hạn SD:<br/>
+                                            <span className="small-text">(Lifetime limit to)</span></p>
+                                        </div>
+                                        </div>
+                                        <div className='row-flex'>
+                                        <p>Kinh doanh vận tải <span className="small-text">(Commercial Use)</span></p>
+                                        <input type='checkbox' checked/>
+                                        <p> Cải tạo <span className="small-text">(Modification)</span></p>
+                                        <input type='checkbox'/>
+                                        </div>
+                                        <h3 className='margintop'>2. THÔNG SỐ KỸ THUẬT <span className="big-text">(SPECIFICATIONS)</span></h3>
+                                        <div className='row-flex'>
+                                        <div className='row-flex-item'>
+                                            <p>Công thức bánh xe: 4x2<br/>
+                                            <span className="small-text">(Wheel Formula)</span></p>
+                                        </div>
+                                        <div className='row-flex-item'>
+                                            <div className='spaceAround'>
+                                                <p>Vết bánh xe:</p> <p className="right-side">1475/1459 (mm)</p>
+                                            </div>
+                                            <p><span className="small-text">(Wheel Tread)</span></p>
+                                        </div>
+                                        </div>
+                                        <div className='spaceAround'>
+                                            <p>Kích thước bao: <span className="small-text">(Overall Dismension)</span></p> <p className="right-side">3817 x 1682 x 1487 (mm)</p>
+                                        </div>
+                                        <div className='spaceAround'>
+                                            <p>KT khoang hành lý lớn nhất</p> <p className="right-side">(mm)</p>
+                                        </div>
+                                        <p><span className="small-text">(Langest luggage container dimension)</span></p>
+                                        <div className='spaceAround'>
+                                            <p>Khối lượng bản thân: <span className="small-text">(Kerb mass)</span></p> <p className="right-side">991 (mm)</p>
+                                        </div>
+                                        <div className='spaceAround'>
+                                            <p>Khối lượng hàng CC theo TK/CP TGGT:</p> <p className="right-side">(kg)</p>
+                                        </div>
+                                        <span className="small-text">(Design/Authorized total mass)</span>
+                                        <div className='spaceAround'>
+                                            <p>Khối lượng kéo theo TK/CP TGGT:</p> <p className="right-side">1366/1366 (kg)</p>
+                                        </div>
+                                        <span className="small-text">(Design/Authorized towed mass)</span>
+                                        <p>Số người cho phép chở: 5 chỗ ngồi, 0 chỗ đứng, 0 chỗ nằm<br/>
+                                        <span className="small-text">(Permissible No. of Pers Carried: seat, stood place, laying place)</span></p>
+                                        <p>Loại nhiên liệu: <span className="small-text">(Type of Fuel Used)</span> Xăng</p>
+                                        <div className='spaceAround'>
+                                            <p>Thể tích làm việc của động cơ: <span className="small-text">(Engine Displacement)</span></p> <p className="right-side">1199 (cm3)</p>
+                                        </div>
+                                        <div className='spaceAround'>
+                                            <p>Công suất lớn nhất/Tốc độ quay: <span className="small-text">(Max. output/rpm)</span></p> <p className="right-side">66(kW)/6000vph</p>
+                                        </div>
+                                        <p>Số seri: <span className="small-text">(No.)</span></p>
+                                    </div>
+                                    <div className='form-row-popup-item outline'>
+                                        <p>Số lượng lốp, cỡ lốp/trục <span className="small-text">(Number of tires, Tires size/axle)</span></p>
+                                        <p>1: 2; 255/55R20</p>
+                                        <p>2: 2; 255/55R20</p>
+                                        <div className='row-flex margintop2'>
+                                            <div className='row-flex-two'>
+                                                <p>Số phiếu kiểm định<br/>
+                                                <span className="small-text">(VehicleInspection Report No)</span><br/>
+                                                5005V-50103/20<br/>
+                                                Có hiệu lức đến hết ngày<br/>
+                                                <span className="small-text">(valid until)</span> <input type='text' value="01/07/2021"/></p>
+                                            </div>
+                                            <div className='row-flex-three'>
+                                                <p>TP.HCM, ngày 2 tháng 1 năm 2020<br/><span className="small-text">(Issued on: Day/Month/Year)</span><br/><h3>ĐƠN VỊ KIỂM ĐỊNH</h3><span className="small-text">(INSPECTION CENTER)</span></p>
+                                            </div>
+                                        </div>
+                                        <img src="https://th.bing.com/th/id/OIP.6Jq8cQRKZB9oMEQLDsE9DAHaEK?pid=ImgDet&rs=1"/>
+                                        <div className='spaceAround'>
+                                            <p>Có lắp thiết bị giám sát hành trình <span className='small-text'>(Equipped with Tachograph)</span></p> <span className="right-side"><input type='checkbox' check/></span>
+                                        </div>
+                                        <div className='spaceAround'>
+                                            <p>Không cấp tem kiểm định <span className='small-text'>(Inspection stamp was not issued)</span></p> <span className="right-side"><input type='checkbox'/></span>
+                                        </div>
+                                        <div className='spaceAround'>
+                                           <p><span className='small-text'>Ghi chú:</span> Biển đăng ký nền trắng</p>
+                                           <i className='bx bxs-file-export' onClick={handlePrint} style={{ cursor: 'pointer', marginLeft: '0.5rem' }}></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
