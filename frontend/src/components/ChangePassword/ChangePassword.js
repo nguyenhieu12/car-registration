741d65d99129f7211ac66facf345a8b0e270a497
NguyenKhaiHoan <banghoan1511@gmail.com>
2023-06-12 04:26:43 +0700

    update change password but can't update

--- frontend/src/components/ChangePassword/ChangePassword.js
+++ frontend/src/components/ChangePassword/ChangePassword.js
@@ -1,110 +1,69 @@
-// import React, {useState} from 'react';
-// import './ChangePassword.css';
-// import 'boxicons/css/boxicons.min.css';
-
-// function ChangePassword(props) {
-//     return (
-//       <div className='change-password-container'>
-//         <div>
-//           <h2 className='change-password-headerText'>Cập nhật tài khoản</h2>
-//           <h4>Cũ</h4>
-//           <div className='form-row'>
-//             <div className="input-container distance">
-//               <label htmlFor="phoneNumber">Tài khoản</label>
-//               <input
-//                 type="text"
-//                 id="phoneNumber"
-//               />
-//             </div>
-//             <div className='input-container'>
-//             </div>
-//           </div>
-//           <div className='form-row'>
-//             <div className="input-container distance">
-//               <label htmlFor="phoneNumber">Mật khẩu khẩu hiện tại</label>
-//               <input
-//                 type="text"
-//                 id="phoneNumber"
-//               />
-//             </div>
-//             <div className='input-container'>
-//             </div>
-//           </div>
-//           <h4>Mới</h4>
-//           <div className='form-row'>
-//             <div className="input-container distance">
-//               <label htmlFor="email">Mật khẩu mới</label>
-//               <input
-//                 type="text"
-//                 id="email"
-//               />
-//             </div>
-//             <div className="input-container">
-//             <label htmlFor="email">Nhập lại mật khẩu</label>
-//             <input
-//               type="text"
-//               id="email"
-//             />
-//           </div>
-//           </div>
-//           <div className="form-row">
-//             <div className="button-container">
-//               <button className='button-update' type="button">
-//                 Cập nhật
-//               </button>
-//               <button className='button-cancel' type="button">
-//                 Hủy bỏ
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div> 
-//     );
-// }
-
-// export default ChangePassword;
-
 import React, { useState, useRef } from 'react';
 import './ChangePassword.css';
 import 'boxicons/css/boxicons.min.css';
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ChangePassword(props) {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  const formRef = useRef();
+
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
   const currentPasswordRef = useRef(null);
   const newPasswordRef = useRef(null);
   const confirmPasswordRef = useRef(null);
 
-
-  const currentUser = localStorage.getItem('currentUser');
-
   const handleUpdate = () => {
-    if (currentPassword !== 'Anhyeuem1234!') {
-      setErrorMessage('Mật khẩu hiện tại không chính xác');
-      return;
+    console.log(currentUser.password);
+    if (currentPassword !== currentUser.user_name) {
+      if (!toast.isActive('checkCurrentPassword')) {
+        toast.error('Mật khẩu hiện tại không chính xác', {
+          toastId: 'checkCurrentPassword',
+          autoClose: 1500,
+        });
+      }
+    } else if (!newPassword) {
+      if (!toast.isActive('checkNull')) {
+        toast.error('Mật khẩu mới không được để trống', {
+          toastId: 'checkNull',
+          autoClose: 1500,
+        });
+      }
+    } else if (newPassword !== confirmPassword) {
+      if (!toast.isActive('checkConfirm')) {
+        toast.error('Mật khẩu không trùng khớp', {
+          toastId: 'checkConfirm',
+          autoClose: 1500,
+        });
+      }
+    } else if (currentPassword !== currentUser.password && newPassword === confirmPassword) {
+      const requirements = validatePassword(newPassword);
+      const isAllRequirementsMet = requirements.every((req) => req.isValid);
+
+      if (isAllRequirementsMet) {
+        if (!toast.isActive('checkAll')) {
+          toast.success('Thay đổi mật khẩu thành công', {
+            toastId: 'checkAll',
+            autoClose: 1500,
+          });
+        }
+      } else {
+        if (!toast.isActive('checkRequirements')) {
+          toast.error('Mật khẩu mới không đáp ứng đủ yêu cầu', {
+            toastId: 'checkRequirements',
+            autoClose: 1500,
+          });
+        }
+      }
     }
-
-    if (newPassword !== confirmPassword) {
-      setErrorMessage('Mật khẩu mới không trùng khớp');
-      return;
-    }
-
-    // Thực hiện cập nhật mật khẩu
-
-    // Reset form
-    setCurrentPassword('');
-    setNewPassword('');
-    setConfirmPassword('');
-    setErrorMessage('');
   };
 
   const handleCancel = () => {
     setCurrentPassword('');
     setNewPassword('');
     setConfirmPassword('');
-    setErrorMessage('');
   };
 
   const handleKeyDown = (event, nextInputRef) => {
@@ -117,12 +76,6 @@ function ChangePassword(props) {
   const handlePasswordChange = (e) => {
     const value = e.target.value;
     setNewPassword(value);
-  };
-
-  const togglePasswordVisibility = () => {
-    const passwordInput = document.getElementById('passwordInput');
-    passwordInput.type =
-      passwordInput.type === 'password' ? 'text' : 'password';
   };
 
   const getRequirements = () => [
@@ -155,14 +108,17 @@ function ChangePassword(props) {
     ));
   };
 
+  const isCurrentPasswordEmpty = currentPassword.length === 0;
+
   return (
     <div className='change-password-container'>
-      <div>
+      <ToastContainer/>
+      <div ref={formRef}>
         <h2 className='change-password-headerText'>Cập nhật tài khoản</h2>
         <div className='form-row'>
           <div className='input-container distance'>
-            <label htmlFor='phoneNumber'>Tài khoản</label>
-            <input type='text' id='phoneNumber' value='123456' disabled />
+            <label htmlFor='username'>Tài khoản</label>
+            <input type='text' id='username' value={currentUser.user_name} disabled />
           </div>
           <div className='input-container'></div>
         </div>
@@ -190,6 +146,8 @@ function ChangePassword(props) {
               onChange={handlePasswordChange}
               onKeyDown={(e) => handleKeyDown(e, confirmPasswordRef)}
               ref={newPasswordRef}
+              title={isCurrentPasswordEmpty ? 'Không thể sửa đổi' : null}
+              disabled={isCurrentPasswordEmpty}
             />
           </div>
           <div className='input-container'>
@@ -201,11 +159,13 @@ function ChangePassword(props) {
               onChange={(e) => setConfirmPassword(e.target.value)}
               onKeyDown={(e) => handleKeyDown(e, currentPasswordRef)}
               ref={confirmPasswordRef}
+              title={isCurrentPasswordEmpty ? 'Không thể sửa đổi' : null}
+              disabled={isCurrentPasswordEmpty}
             />
           </div>
         </div>
         <div className="wrapper">
-          <div className="content">
+          <div className="content-rule-password">
             <div>Điều kiện: </div>
             <ul className="requirement-list">
               {renderRequirements()}
@@ -214,15 +174,12 @@ function ChangePassword(props) {
         </div>
         <div className='form-row'>
           <div className='button-container'>
-            <button className='button-update' type='button' onClick={handleUpdate}>
-              Cập nhật
-            </button>
-            <button className='button-cancel' type='button' onClick={handleCancel}>
-              Hủy bỏ
-            </button>
+              <label htmlFor='button-u' className='app-content-headerButton-u'>Cập nhật</label>
+              <button id='button-u' className='button-update' type="button" onClick={handleUpdate} style={{ 'display': 'none' }} disabled={isCurrentPasswordEmpty} />
+              <label htmlFor='button-c' className='app-content-headerButton-c'>Hủy</label>
+              <button id='button-c' className='button-cancel' type="button" onClick={handleCancel} style={{ 'display': 'none' }} disabled={isCurrentPasswordEmpty} />
           </div>
         </div>
-        {errorMessage && <div className='error-message'>{errorMessage}</div>}
       </div>
     </div>
   );
