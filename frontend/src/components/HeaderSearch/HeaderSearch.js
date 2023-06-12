e60879d141e1a0e64832f74032205d40aa3f485a
NguyenKhaiHoan <banghoan1511@gmail.com>
2023-06-13 06:59:33 +0700

    update

--- frontend/src/components/HeaderSearch/HeaderSearch.js
+++ frontend/src/components/HeaderSearch/HeaderSearch.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HeaderSearch.css';
+import Timeline from '../Timeline/Timeline';
 
 function HeaderSearch(props) {
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+
+    const [pair, setPair] = useState([]);
+    const [vehicle, setVehicle] = useState([]);
+
+    const getHistoryInspections = async (value1, value2) => {
+        const payload = {
+            person_identity_number: value1,
+            registration_id: value2,
+        };
+        fetch(`http://localhost:5000/api/v1/visitor`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(payload)
+        })
+            .then(response => response.json())
+            .then(data => {
+              setPair(data.data.pair);
+              setVehicle(data.data.vehicle);
+            })
+            .catch(error => {
+              console.error(error);
+            });
+    };
+
+    const handleChange = () => {
+        getHistoryInspections(input1, input2);
+    }
+      
     return (
-        <div className='search-container'>
+        <div>
+            <div className='search-container'>
             <div className="layout">
                 <div className="layout-text">
                     <h1 className="title">
@@ -18,18 +52,20 @@ function HeaderSearch(props) {
                         <label htmlFor="cccd">
                             Nhập Căn Cước Công Dân hoặc Mã số thuế
                         </label>
-                        <input type="text" placeholder="Ví dụ: 012345678910"></input>
+                        <input type="text" placeholder="Ví dụ: 012345678910" onChange={(e) => setInput1(e.target.value)}></input>
                     </div>
 
                     <div className="carNumberDiv">
                         <label htmlFor="carNumber">
                             Nhập biển số xe
                         </label>
-                        <input type="text" placeholder="Ví dụ: 12A-34567"></input>
+                        <input type="text" placeholder="Ví dụ: 12A-34567" onChange={(e) => setInput2(e.target.value)}></input>
                     </div>
-                    <button className='btn'>Tìm kiếm</button>
+                    <button className='btn' onClick={handleChange}>Tìm kiếm</button>
                 </div>
             </div>
+        </div>
+        <Timeline pair={pair} vehicle={vehicle}/>
         </div>
     );
 }
