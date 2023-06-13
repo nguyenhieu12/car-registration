fcfe1bb458bb061a6410429e06d1381cd3913e38
nguyenhieu12 <vailinhhon098@gmail.com>
2023-06-13 15:17:29 +0700

    add dashboard content statistical

--- frontend/src/components/Charts/PieChart/PieChart.js
+++ frontend/src/components/Charts/PieChart/PieChart.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Radar } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 import "chartjs-plugin-datalabels";
 import "./PieChart.css";
 
 function PieChart({ chartData, chartOptions }) {
     return (
         <div className="pie-chart-container">
-            <Radar data={chartData} options={chartOptions} />
+            <Pie data={chartData} options={chartOptions} />
         </div>
     );
 }
--- frontend/src/components/DashboardContent/DashboardContent.js
+++ frontend/src/components/DashboardContent/DashboardContent.js
@@ -8,7 +8,7 @@ import 'boxicons/css/boxicons.min.css';
 function DashboardContent(props) {
     const token = localStorage.getItem('token');
 
-    const [selectedDateOption, setSelectedDateOption] = useState('year');
+    const [selectedStatisticalOption, setSelectedStatisticalOption] = useState('cars');
 
     const [selectedChartOption, setSelectedChartOption] = useState('bar');
 
@@ -38,67 +38,137 @@ function DashboardContent(props) {
         return colors;
     }
 
+    const carsByYears = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/v1/insp/all?size=3273&orderBy=inspection_date', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
+            if (response.ok) {
+                const data = await response.json();
+                const inspections = data.data.inspections;
+                const carData = inspections.reduce((result, inspection) => {
+                    const inspectionYear = new Date(inspection.inspection_date).getFullYear();
+                    const existingObj = result.find(obj => obj.year === inspectionYear);
+                    if (existingObj) {
+                        existingObj.car++;
+                    } else {
+                        result.push({ year: inspectionYear, car: 1 });
+                    }
+                    return result;
+                }, []);
 
-    useLayoutEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:5000/api/v1/insp/all?size=48&orderBy=inspection_date', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
+                setCarInspection(carData.reduce((cars, currentQuantity) => cars + currentQuantity.car, 0));
 
-                if (response.ok) {
-                    const data = await response.json();
-                    const inspections = data.data.inspections;
-                    const carData = inspections.reduce((result, inspection) => {
-                        const inspectionYear = new Date(inspection.inspection_date).getFullYear();
-                        const existingObj = result.find(obj => obj.year === inspectionYear);
-                        if (existingObj) {
-                            existingObj.car++;
-                        } else {
-                            result.push({ year: inspectionYear, car: 1 });
-                        }
-                        return result;
-                    }, []);
-
-                    setCarInspection(carData.reduce((cars, currentQuantity) => cars + currentQuantity.car, 0));
-
-                    setCarExpiry(carData.reduce((cars, currentQuantity) =>
+                setCarExpiry(carData.reduce((cars, currentQuantity) =>
                         ((currentQuantity.year >= 2021 && currentQuantity.year < 2023) ? (cars + currentQuantity.car) : cars)
                     , 0));
 
-                    setChartData(
-                        {
-                            labels: carData.map(data => data.year),
-                            datasets: [
-                                {
-                                    label: "Số lượng",
-                                    data: carData.map(data => data.car),
-                                    backgroundColor: generateRandomColors(carData.length),
-                                    borderWidth: 1,
-                                    borderColor: "black"
-                                }
-                            ]
-                        }
-                    );
-                } else {
-                    console.log('Error:', response.status);
-                }
-            } catch (error) {
-                console.error('Error:', error);
+                setChartData(
+                    {
+                        labels: carData.map(data => data.year),
+                        datasets: [
+                            {
+                                label: "Số lượng",
+                                data: carData.map(data => data.car),
+                                backgroundColor: generateRandomColors(carData.length),
+                                borderWidth: 0.5,
+                                borderColor: "black"
+                            }
+                        ]
+                    }
+                );
+            } else {
+                console.log('Error:', response.status);
             }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
 
-        };
+    const carsByQuarters = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/v1/insp/statistic/all', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
-        fetchData();
-    }, []);
+            if (response.ok) {
+                const responseData = await response.json();
+                const data = responseData.data;
 
-    const handleDateSelectChange = (event) => {
-        setSelectedDateOption(event.target.value);
+                setChartData(
+                    {
+                        labels: ['Quý 1', 'Quý 2', 'Quý 3', 'Quý 4'],
+                        datasets: data.map((item) => ({
+                            label: item.year.toString(),
+                            data: Object.values(item.cars_count),
+                            backgroundColor: generateRandomColors(4),
+                        })),
+                    }
+                );
+            } else {
+                console.log('Error:', response.status);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const carsByRegions = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/v1/insp/statistic/region', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                const responseData = await response.json();
+                const data = responseData.data;
+                console.log(data);
+
+                setChartData(
+                    {
+                        labels: ['Miền Bắc', 'Miền Trung', 'Miền Nam'],
+                        datasets: data.map((item) => ({
+                            label: item.year.toString(),
+                            data: Object.values(item.cars_count_region),
+                            backgroundColor: generateRandomColors(4),
+                        })),
+                    }
+                );
+            } else {
+                console.log('Error:', response.status);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    useLayoutEffect(() => {
+
+        if (selectedStatisticalOption === 'cars') {
+            carsByYears();
+        } else if (selectedStatisticalOption === 'quarters') {
+            carsByQuarters();
+        } else {
+            carsByRegions();
+        }
+
+    }, [selectedStatisticalOption]);
+
+    const handleStatisticalSelectChange = (event) => {
+        setSelectedStatisticalOption(event.target.value);
     };
 
     const handleChartSelectChange = (event) => {
@@ -111,10 +181,6 @@ function DashboardContent(props) {
         responsive: true,
         maintainAspectRatio: false,
         plugins: {
-            legend: {
-                position: 'right'
-
-            },
             title: {
                 display: true,
                 align: 'end'
@@ -125,15 +191,15 @@ function DashboardContent(props) {
     return (
         <div className="dashboard-content">
             <div className='stats-container'>
-                <div className="stats-option">
+                <div className="stats-option by-cars">
                     <span>Thống kê theo</span>
-                    <select value={selectedDateOption} onChange={handleDateSelectChange}>
-                        <option value="day">Ngày</option>
-                        <option value="month">Tháng</option>
-                        <option value="year">Năm</option>
+                    <select value={selectedStatisticalOption} onChange={handleStatisticalSelectChange}>
+                        <option value="cars">Tổng lượng xe</option>
+                        <option value="quarters">Quý</option>
+                        <option value="regions">Miền</option>
                     </select>
                 </div>
-                <div className="stats-option">
+                <div className="stats-option by-chart">
                     <span>Loại biểu đồ</span>
                     <select value={selectedChartOption} onChange={handleChartSelectChange}>
                         <option value="bar">Cột</option>
@@ -142,18 +208,17 @@ function DashboardContent(props) {
                     </select>
                 </div>
                 <div className='stats-quantity car-inspection'>
-                    <div className='bx bx-car stats-icon car-icon'></div>
+                    <div className='bx bx-car stats-icon'></div>
                     <div className='stats-content'>
-                        <h3>Xe đã đăng kiểm</h3>
-                        <strong>{carInspection}</strong>
-
+                        <h3>{carInspection}</h3>
+                        <p>Xe đã đăng kiểm</p>
                     </div>
                 </div>
                 <div className='stats-quantity car-expiry'>
-                    <div className='bx bx-time-five stats-icon time-icon'></div>
+                    <div className='bx bx-time-five stats-icon'></div>
                     <div className='stats-content'>
-                        <h3>Xe sắp đến hạn</h3>
-                        <strong>{carExpiry}</strong>
+                        <h3>{carExpiry}</h3>
+                        <p>Xe sắp đến hạn</p>
                     </div>
                 </div>
             </div>
